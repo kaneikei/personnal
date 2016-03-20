954568c36687e796572508c29c5b9df1e7e39ea2
kaneikei <646827576@qq.com>
2016-03-20 14:42:00 +0800

    gaigaodu

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ $(function()
 
 	})()
 	var t=setInterval(move,4000);
-	var jieshao="我是李海涛，来自山西，是一名前端工程师";
+	var jieshao="我是李海涛，是一名前端工程师。热爱生活，热爱前端，喜欢漫画，喜欢音乐。享受按照客户要求完成作品的过程，享受这一刻的成就感。相信努力就有收获，双手创造明天！";
 	var $ziwojieshao=$('.erloubox .jutijianjie');
 	$ziwojieshao.text();
 	var zichuxian=(function()
